3cd756c2159cb02c603ad9a8609ae2d6868ee18a
sbs24011 <sbs24011@student.cct.ie>
2024-05-21 21:24:08 +0100

    new title

--- Dairy_tarde_dash.py
+++ Dairy_tarde_dash.py
@@ -15,14 +15,23 @@ def plot_data(data, title, year):
     chart = px.bar(summary, x='Quantityintonnes', y='Partner', orientation='h', title=title)
     st.plotly_chart(chart)
 
+
 uploaded_imports = st.file_uploader("Upload Ireland Imports CSV", type="csv")
 uploaded_exports = st.file_uploader("Upload Ireland Exports CSV", type="csv")
-selected_year = st.selectbox("Select Year", [2021, 2022, 2023])
+
+selected_year = st.selectbox("Select Year", [2018,2019,2020,2021, 2022, 2023])
+
 
 imports_data = load_data(uploaded_imports)
 exports_data = load_data(uploaded_exports)
 
+
 if imports_data is not None and exports_data is not None:
     st.header("Dairy Imports and Exports Analysis")
+    
+    # Subheader for ProductGroups
+    st.subheader("ProductGroups Imported and Exported by Year")
+    
+    # Plot imports and exports data
     plot_data(imports_data, f"Dairy Imports by Partner ({selected_year})", selected_year)
     plot_data(exports_data, f"Dairy Exports by Partner ({selected_year})", selected_year)
