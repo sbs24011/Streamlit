b5a237ffd66e2604856c72faa9133eed3ee24035
sbs24011 <sbs24011@student.cct.ie>
2024-05-26 13:01:21 +0100

    Revert test

--- Dairy_tarde_dash.py
+++ Dairy_tarde_dash.py
@@ -69,7 +69,7 @@ if best_prediction_df is not None:
 
     selected_country = st.selectbox("Select Country", best_prediction_df['Partner'].unique())
     selected_limit = st.selectbox("Select Limit", [5, 10, 20, -1], format_func=lambda x: "No limit" if x == -1 else f"Top {x} products")
-    selected_month = st.slider("Select Month", 0, len(unique_months) - 1, 0, format=lambda x: month_names[unique_months[x]])
+    selected_month = st.slider("Select Month", 0, len(unique_months) - 1, 0, format_func=lambda x: month_names[unique_months[x]])
 
     filtered_df = best_prediction_df[
         (best_prediction_df['month'] == unique_months[selected_month]) &
